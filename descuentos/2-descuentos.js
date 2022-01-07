5de8fd48866b656dd473d7085c2a344c789fe368
Ernique <octepc.97@gmail.com>
2022-01-07 14:21:22 -0500

    finished the challenges

--- descuentos/2-descuentos.js
+++ descuentos/2-descuentos.js
@@ -1,19 +1,32 @@
+const coupons = ["coupon1", "coupon2", "coupon3"];
+
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajePrecioConDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
-
   return precioConDescuento;
 }
 
 function priceDiscount() {
   const inputPrice = document.getElementById("InputPrice");
   const priceValue = Number(inputPrice.value);
-  const inputDiscount = document.getElementById("InputDiscount");
-  const discountValue = Number(inputDiscount.value);
-  const precioConDescuento = calcularPrecioConDescuento(
-    priceValue,
-    discountValue
-  );
+  const inputCoupon = document.getElementById("InputCoupon");
+  const couponValue = inputCoupon.value;
+
+  let descuento;
+
+  if (!coupons.includes(couponValue)) {
+    alert("El cupón " + couponValue + " no es válido");
+  } else if (couponValue === "coupon1") {
+    descuento = 15;
+  } else if (couponValue === "coupon2") {
+    descuento = 30;
+  } else if (couponValue === "coupon3") {
+    descuento = 25;
+  }
+
+  console.log(coupons[0]);
+
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
   const resultP = document.getElementById("ResultPice");
   resultP.innerHTML = "El precio don descuento es: $" + precioConDescuento;
 }
